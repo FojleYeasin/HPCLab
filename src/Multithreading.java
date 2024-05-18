92cb08bd7f4098df5ce29fba310373554f290821
FojleYeasin <baikka.khullam2@gmail.com>
2024-05-18 09:48:17 +0600

    Add files via upload

---
+++ src/Multithreading.java
@@ -0,0 +1,13 @@
+public class Multithreading extends Thread{
+    // @override
+    public void run() {
+        for (int i=1; i<=5; i++) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
